f498256f7173fdfdccd4b443c6d4d06f7548093e
Hadi <hvahabpoor@gamil.com>
2020-09-05 22:39:26 +0200

    add JPA module to intellije module use persistance to map whatevere it is in databse to entity package. create persistance.xml file in the project structure/module.

---
+++ src/main/java/entity/HumanEntity.java
@@ -0,0 +1,48 @@
+package entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "human", schema = "public", catalog = "postgres")
+public class HumanEntity {
+    private String name;
+    private int id;
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HumanEntity that = (HumanEntity) o;
+        return id == that.id &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+}
